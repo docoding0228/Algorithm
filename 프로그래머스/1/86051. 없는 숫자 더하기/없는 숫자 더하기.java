2d3fceef6a4f26c272d9c948687393017cba6a2e
docoding0228 <165769964+docoding0228@users.noreply.github.com>
2024-05-19 13:46:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.71 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,14 +1,32 @@
+// class Solution {
+//     public int solution(int[] numbers) {
+//         int answer = 0;
+//         int[] answ = new int[10];
+        
+//         for(int i=0; i<numbers.length; i++) {
+//             answ[numbers[i]] = 1;
+//         }
+//         for(int i=0; i<answ.length; i++) {
+//             if(answ[i] == 0) answer += i;
+//         }
+//         return answer;
+//     }
+// }
+
+// class Solution {
+//     public int solution(int[] numbers) {
+//         int sum = 45;
+//         for (int i : numbers) {
+//             sum -= i;
+//         }
+//         return sum;
+//     }
+// }
+
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int[] answ = new int[10];
-        
-        for(int i=0; i<numbers.length; i++) {
-            answ[numbers[i]] = 1;
-        }
-        for(int i=0; i<answ.length; i++) {
-            if(answ[i] == 0) answer += i;
-        }
-        return answer;
+        return 45-Arrays.stream(numbers).sum();
     }
 }
