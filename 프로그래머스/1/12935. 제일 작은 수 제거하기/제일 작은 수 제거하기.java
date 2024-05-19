6434f8a33f591f9e45082c8e5f440d0cc3feb439
docoding0228 <165769964+docoding0228@users.noreply.github.com>
2024-05-20 00:20:41 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.92 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,25 +1,52 @@
+// class Solution {
+//     public int[] solution(int[] arr) { 
+//         if(arr.length == 1){
+//             int[] answer = {-1};
+//             return answer;
+//         }
+        
+//         int min = arr[0];
+//         int[] answer = new int[arr.length -1];
+        
+//         for(int i=0; i<arr.length; i++){
+//             min = Math.min(min,arr[i]);
+//         }
+        
+//         int idx = 0;
+//         for(int i=0; i<arr.length; i++){
+//             if(min == arr[i]){
+//                 continue;
+//             }else{
+//             	answer[idx++] = arr[i];
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
+// import java.util.*;
+// import java.util.stream.Collectors;
+
+// class Solution {
+//     public int[] solution(int[] arr) {
+//         List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+//         Arrays.sort(arr);
+//         list.remove(list.indexOf(arr[0]));
+//         if (list.size() <= 0) return new int[]{-1};
+//         return list.stream().mapToInt(i->i).toArray();
+//     }
+// }
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
-    public int[] solution(int[] arr) { 
-        if(arr.length == 1){
-            int[] answer = {-1};
-            return answer;
-        }
-        
-        int min = arr[0];
-        int[] answer = new int[arr.length -1];
-        
-        for(int i=0; i<arr.length; i++){
-            min = Math.min(min,arr[i]);
-        }
-        
-        int idx = 0;
-        for(int i=0; i<arr.length; i++){
-            if(min == arr[i]){
-                continue;
-            }else{
-            	answer[idx++] = arr[i];
-            }
-        }
-        return answer;
-    }
+  public int[] solution(int[] arr) {
+      if (arr.length <= 1) return new int[]{ -1 };
+      int min = Arrays.stream(arr).min().getAsInt();
+      return Arrays.stream(arr).filter(i -> i != min).toArray();
+  }
 }
+
