6c2ae14d60f1c98bdf8666dd1e2e789928fed396
docoding0228 <165769964+docoding0228@users.noreply.github.com>
2024-05-22 15:18:57 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.08 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,4 +1,4 @@
-// class Solution {
+//class Solution {
 //  public String solution(int n) {
 //    String answer = "";
  
@@ -10,12 +10,33 @@
 //   }
 // }
 
-class Solution {
- public String solution(int n) {
-    String answer = "";
-        for(int i=1; i<=n; i++) {
-    	    answer += i%2!=0 ? "수" : "박";
+// class Solution {
+//     public String solution(int n) {
+//         String answer = "";
+//         for(int i=1; i<=n; i++) {
+//             answer += i%2!=0 ? "수" : "박";
+//         }
+//         return answer;
+//     }
+// }
+
+// class Solution {
+//     public String solution(int n){
+//         String result = "";
+        
+//         for(int i=0;i<n;i++) {
+//             result +=(i%2==0)? "수":"박";
+//         }
+        
+//         return result;
+//     }
+// }
+
+public class Solution {
+    public String solution(int n)
+    {
+        return new String(new char [n/2+1]).replace("\0", "수박").substring(0,n);
     }
-    return answer;
-  }
 }
+
+
