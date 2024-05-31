e7f69247043ab1b68ddee4c3edfcb2dea5bae44a
docoding0228 <165769964+docoding0228@users.noreply.github.com>
2024-06-01 03:43:23 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.09 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,20 +1,33 @@
+// class Solution {
+//     public int[] solution(int n, int m) {
+//         int[] answer = new int[2];
+        
+//         int max = Math.max(n, m);
+//         int min = Math.min(n, m);
+        
+//         answer[0] = getGcd(max, min);
+//         answer[1] = max * min / answer[0];
+        
+//         return answer;
+//     }
+    
+//     public int getGcd(int max, int min) {
+
+//         if(max%min == 0) return min;
+//         else return getGcd(min, max%min);
+
+//     }
+// }
+
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
-        
-        int max = Math.max(n, m);
-        int min = Math.min(n, m);
-        
-        answer[0] = getGcd(max, min);
-        answer[1] = max * min / answer[0];
-        
+        for (int i = 1; i < n + m; i++) {
+            if (n % i == 0 && m % i == 0) {
+                answer[0] = i;
+                answer[1] = n * m / answer[0];
+            }
+        }
         return answer;
-    }
-    
-    public int getGcd(int max, int min) {
-
-        if(max%min == 0) return min;
-        else return getGcd(min, max%min);
-
     }
 }
