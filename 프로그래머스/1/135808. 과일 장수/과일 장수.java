cc089bd20491572dcefb0d55df9b604d7839fd6c
docoding0228 <165769964+docoding0228@users.noreply.github.com>
2024-06-13 01:34:32 +0900

    [level 1] Title: 과일 장수, Time: 0.41 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int answer = 0;
+        for(int i=0;i<score.length;i++) {
+            pq.add(score[i]);
+        }
+        while(pq.size() >= m) {
+            for(int i=0;i<m;i++) {
+                if (i == m-1) {
+                    answer += (pq.poll() * m);
+                } else {
+                    pq.poll();
+                }
+            }
+        }
+        return answer;
+    }
+}
