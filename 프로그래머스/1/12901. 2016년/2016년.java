a6f9669ee98f5f33390695d57c82c3b3ced94cf9
docoding0228 <165769964+docoding0228@users.noreply.github.com>
2024-06-13 23:27:41 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -2,7 +2,7 @@ class Solution {
     public String solution(int a, int b) {
         String[] days = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
         int[] months = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30};
-        int total = 0; // 총 일수
+        int total = 0;
         
         for(int i=0; i<a; i++) {
             total += months[i];
